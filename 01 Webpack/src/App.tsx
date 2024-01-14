15b03c27b1b20f48723ea60c07c07dc9688cac75
Michal Radomski <michalra@op.pl>
2024-01-14 18:11:33 +0100

    Styled-components added

--- 01 Webpack/src/App.tsx
+++ 01 Webpack/src/App.tsx
@@ -1,11 +1,28 @@
 import React from "react";
+import styled from "styled-components";
 
 import "./App.scss";
+// @ts-ignore
+import variableColors from "./_App.module.scss";
+
+const { primaryColor, dangerColor } = variableColors;
+
+const HeaderContainer = styled.div<{ $color: string }>`
+  border: 1px solid;
+  border-color: ${(props) => props.$color};
+  & p.custom-text {
+    color: ${dangerColor};
+  }
+`;
 
 function App(): JSX.Element {
   return (
     <React.Fragment>
-      <h1 className="text-center text-muted">App</h1>
+      <HeaderContainer $color={primaryColor}>
+        <h1 className="text-center">App Header</h1>
+        <p className="text-muted">App paragraph - text-muted</p>
+        <p className="custom-text">App paragraph - custom-text</p>
+      </HeaderContainer>
     </React.Fragment>
   );
 }
--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Custom apps starters
 
-1. Custom Webpack 5 config: Babel, ReactJS, Sass and TypeScript
+1. Custom Webpack 5 config: Babel, ReactJS, Sass, TypeScript and Styled-Components
