e43773edbf8b215a1d7e9ca0ab3588452663d2c8
Michal Radomski <michalra@op.pl>
2024-01-17 01:44:39 +0100

    Webpack with Express and TS custom config - webpack config added

---
+++ 05 Webpack-Express-TS/webpack.config.ts
@@ -0,0 +1,24 @@
+import path from "path";
+import webpack from "webpack";
+
+const config: webpack.Configuration = {
+  mode: "production",
+  devtool: "source-map",
+  entry: { main: "./server.ts" },
+  output: { path: path.join(__dirname, "/dist"), filename: "bundleApi.js", clean: true, publicPath: "", iife: true },
+  target: "node",
+  optimization: {
+    minimize: true,
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".json"],
+  },
+  module: {
+    rules: [
+      { test: /\.ts$/, use: "ts-loader", exclude: /node_modules/ },
+      { test: /\.(svg)$/, type: "asset/inline" },
+    ],
+  },
+};
+
+export default config;
