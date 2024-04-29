712b6f6d32166f1f6d9b6a834f5319db4a181104
Michal Radomski <michalra@op.pl>
2024-04-29 16:27:31 +0200

    Redux added2

--- 02 Vite-React-Redux-TS/src/main.tsx
+++ 02 Vite-React-Redux-TS/src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 
 import App from "./App.tsx";
+import store from "./redux/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
