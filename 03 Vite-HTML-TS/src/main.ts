7ac771344dae3319510050df51661ce85e2aaffc
Michal Radomski <michalra@op.pl>
2024-05-12 23:35:28 +0200

    Litte update

--- 03 Vite-HTML-TS/src/main.ts
+++ 03 Vite-HTML-TS/src/main.ts
@@ -1,3 +1,13 @@
 import "./style.scss";
 
+declare global {
+  interface Window {
+    $: JQueryStatic;
+  }
+}
+
 console.log("Vite testing");
+
+$(document).ready(function () {
+  $("h1").css("background-color", "#b3ffff");
+});
