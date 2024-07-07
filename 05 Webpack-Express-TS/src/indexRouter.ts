fce19e0e401855ea1369c99071bba3e389d3504b
Michal Radomski <michalra@op.pl>
2024-07-07 22:11:52 +0200

    Router options added

--- 05 Webpack-Express-TS/src/indexRouter.ts
+++ 05 Webpack-Express-TS/src/indexRouter.ts
@@ -2,7 +2,7 @@ import express, { Router } from "express";
 
 import { getUsers, getWholeList } from "./controller";
 
-const indexRouter: Router = express.Router();
+const indexRouter: Router = express.Router({ strict: false, caseSensitive: true });
 
 //* MongoDB - list of users
 indexRouter.get("/mongo-users", getUsers);
