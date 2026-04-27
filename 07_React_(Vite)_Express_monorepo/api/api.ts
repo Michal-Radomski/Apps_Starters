0bac5e69d34affaa202b2fd4ceb18097f3abe2dc
Michal Radomski <michalra@op.pl>
2026-04-27 20:59:39 +0200

    Litte update

--- 07_React_(Vite)_Express_monorepo/api/api.ts
+++ 07_React_(Vite)_Express_monorepo/api/api.ts
@@ -1,5 +1,6 @@
 // Todo: Update security according to: https://github.com/Michal-Radomski/React-Node-courses/blob/main/03%20Advanced%20Node/02/server/server.ts
 // Todo: add nodemon.json
+// Todo: Update CSP, add XSS and CSRF protection
 
 import path from "path";
 import http from "http";
